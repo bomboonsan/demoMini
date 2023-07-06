4de0346e7643ca1cf768b5eb2395c24b90af7da6
bomboonsan <bomboonsan@gmail.com>
2023-07-06 23:21:04 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
           // const isFriend = await liff.isFriend(liff.getOS(), userId);
           const isFriendData = await liff.getFriendship();
           const isFriend = isFriendData.friendFlag
-          console.log('isFriendData'+isFriendData)
+          console.log(isFriendData)
           if (isFriend) {
             // window.location.href = liffUrl;
           } else {
